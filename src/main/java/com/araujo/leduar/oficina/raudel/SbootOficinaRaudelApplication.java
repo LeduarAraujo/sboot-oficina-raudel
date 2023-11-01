e511f7a0ee955bac0e7fe2da130223fdf01c7596
leduararaujo <leduar.araujo@hotmail.com>
2023-10-31 23:18:11 -0300

    Criação do projeto

---
+++ src/main/java/com/araujo/leduar/oficina/raudel/SbootOficinaRaudelApplication.java
@@ -0,0 +1,13 @@
+package com.araujo.leduar.oficina.raudel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootOficinaRaudelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbootOficinaRaudelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/araujo/leduar/oficina/raudel/SbootOficinaRaudelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.araujo.leduar.oficina.raudel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootOficinaRaudelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
